0ee8fa8c640b3b3e45b7476c04fad02604358cba
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-11 21:11:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,4 @@
 def prime?(number)
-  if number <= 1
-     false
-  else
-  i = 2
-  while i < number
-    if number % i == 0
-      false
-      i += 1
-    else
-      true
-      i = number
-  end
+  (2..math.sqrt(number)).each {|i| return false if number % i == 0}
+  true
 end
