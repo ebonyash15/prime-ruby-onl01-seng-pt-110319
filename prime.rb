91371363a3d82af3f637629208f1d0d9b0edffdc
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-11 21:01:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+     false
+  else
+  i = 2
+  while i < number
+    if number % i == 0
+      false
+      i += 1
+    else
+      true
+      i = number
+  end
+end
